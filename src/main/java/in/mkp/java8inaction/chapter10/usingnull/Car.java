2ba7c950765c630c6d95f3df03fc043daa406904
Mahesh Kumar <mkumar@videologygroup.com>
2018-08-20 15:43:06 -0400

    Adding missed files

---
+++ src/main/java/in/mkp/java8inaction/chapter10/usingnull/Car.java
@@ -0,0 +1,10 @@
+package in.mkp.java8inaction.chapter10.usingnull;
+
+public class Car {
+
+    public Insurance insurance;
+
+    public Insurance getInsurance() {
+        return this.insurance;
+    }
+}
---
+++ src/main/java/in/mkp/java8inaction/chapter10/usingnull/Person.java
@@ -0,0 +1,10 @@
+package in.mkp.java8inaction.chapter10.usingnull;
+
+public class Person {
+
+    private Car car;
+
+    public Car getCar() {
+        return this.car;
+    }
+}
